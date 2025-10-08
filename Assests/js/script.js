dacce69aa68b1599e829438e6c0aec13ebb4da66
Praveen Wijewardana <praveenwijewardana1@gmail.com>
2025-10-08 12:17:15 +0530

    Fist Commit

---
+++ Assests/js/script.js
@@ -0,0 +1,37 @@
+console.log("js loaded.......");
+function btn1(){
+    var text = document.getElementById("h1");
+    alert(text.value);
+
+    //var
+    //let
+    //const
+
+    //var a;
+    //var b;
+    //var c=3.14;
+
+    //console.log("Var" + a);
+    //console.log("let" + a);    
+    //console.log("const" + a);
+
+    //a=10;
+    //b="kamal"
+    // c=2;t
+
+
+    let a = 10;
+    let b = "Kamal";
+    let c = true;
+    let d = null
+    let e;
+    let f = {"name": "Amarapala", "age": 10}
+console. log("a = 10; " + typeof a);
+console.log("b = Kamal; " + typeof b);
+console.log("c = true " + typeof c);
+console.log("d = null " + typeof d);
+console.log("let e; " + typeof e); console.log( typeof f);
+
+
+
+}
